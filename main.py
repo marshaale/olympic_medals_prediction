0e621ba30aed89def08d41178e82def3394ed7ea
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-07-21 20:23:18 +0300

    implement cors

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Olympic Medal Prediction API",
     version="1.0.0",
     description="Predict Olympic medals and country strength.",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
