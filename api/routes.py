553f7a6c2d43a8fa7c7f16f652e29275187125c3
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-07-21 17:31:43 +0300

    implement predict route

---
+++ api/routes.py
@@ -0,0 +1,17 @@
+from helpers.predict import predict_medals_noc_strength
+from schemas.user_input import UserInput,PredictionResponse
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter()
+
+# ----------------------------
+# Routes
+# ----------------------------
+
+@router.post("/predict", response_model=PredictionResponse)
+def predict(data: UserInput):
+    try:
+        return predict_medals_noc_strength(data)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
