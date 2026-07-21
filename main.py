58784e7b76921cea21257a5d6fbbf01dfd2577b4
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-07-21 17:32:55 +0300

    register api routes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from api.routes import router
 
 app = FastAPI(
     title="Olympic Medal Prediction API",
@@ -6,9 +7,10 @@ app = FastAPI(
     description="Predict Olympic medals and country strength.",
 )
 
+
 @app.get("/")
 def root():
-    return {
-        "message": "Olympic Medal Prediction API",
-        "status": "running"
-    }
+    return {"message": "Olympic Medal Prediction API", "status": "running"}
+
+
+app.include_router(router, prefix="/api/v1")
